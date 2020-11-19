598cccbe1849a9c78fe448e8b4d3a9aa07c5a813
Japleen1111 <japleen.juneja@gmail.com>
2020-11-19 22:51:52 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -6,6 +7,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 
+var platform;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -35,7 +37,6 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
 }
 
 function draw(){
